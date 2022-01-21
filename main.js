cbe1ae26b51b76dfdd3cc20c26e64808ccbca317
BunnyMeee <75510733+BunnyMeee@users.noreply.github.com>
2022-01-20 17:24:57 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function takeSnapshot() {
 
 }
 console.log('ml5 version :', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/igGnrZRAo/', modelloaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/igGnrZRAo/model.json', modelloaded);
 function modelloaded() {
     console.log('modelloaded');
 
@@ -26,4 +26,40 @@ function speak() {
     speak_data = "The Prediction is " + prediction;
     var utterthis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterthis);
+}
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        prediction1 = results[0].label;
+        speak();
+        if (results[0].label == "Victory") {
+            document.getElementById("update_emoji").innerHTML = "✌";
+        }
+        if (results[0].label == "Best") {
+            document.getElementById("update_emoji").innerHTML = "👍";
+        }
+        if (results[0].label == "Amazing") {
+            document.getElementById("update_emoji").innerHTML = "👌";
+        }
+        if (results[0].label == "Down/Bad") {
+            document.getElementById("update_emoji").innerHTML = "👎";
+        }
+        if (results[0].label == "Stop") {
+            document.getElementById("update_emoji").innerHTML = "✋";
+        }
+        if (results[0].label == "Help") {
+            document.getElementById("update_emoji").innerHTML = "✊";
+        }
+        if (results[0].label == "Okay") {
+            document.getElementById("update_emoji").innerHTML = " ";
+        }
+    }
 }
